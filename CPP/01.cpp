feacdac79c057c36e363b4045409295182a3d894
Manver Singh <manveersingh0501l@gmail.com>
2026-06-22 11:49:26 +0530

    added day1 topics

---
+++ CPP/01.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    // int age;
+    // cin>>age;
+    // int number;
+    // cin>>number;
+    // if(age>=18 && number >=10){
+    //     cout<<"Eligible";
+    // }
+    // else{
+    //     cout<<"Not eligible";
+    // }
+
+
+
+    // char ch;
+    // cin>>ch;
+    // switch(ch){
+    //     case 'A':
+    //     cout<<"Excellent"<<endl;
+    //     case 'B':
+    //     cout<<"Good"<<endl;
+    //     case 'C':
+    //     cout<<"Average"<<endl;
+    //     default:
+    //     cout<<"Invalid"<<endl;
+    // }
+
+    int x = 10, y = 20;
+    cout<<(x>y?x:y) <<endl;
+
+}
