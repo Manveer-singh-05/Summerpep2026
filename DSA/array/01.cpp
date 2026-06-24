bfc63483b63b4c21aad04525dcb945654da1a883
Manver Singh <manveersingh0501l@gmail.com>
2026-06-24 11:35:21 +0530

    added time complexity

---
+++ DSA/array/01.cpp
@@ -0,0 +1,7 @@
+// time complexity 
+
+// 1. o(1) = constant 
+// 2. o(logn) = it is considered when we eleminate half of our search array or problem ||  space like in binary search 
+// 3. o(n) = when we traverse the whole array
+// 4. o(nlogn) = it is the mixture of logarithm and traverse the array like merge sort
+//5. o(n2) = it is the qudratic time and most worst we almost avoid this approach 
