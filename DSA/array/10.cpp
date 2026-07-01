76e4cc38b73dad8149e6b442fa0dcb12daaf4e39
Manver Singh <manveersingh0501l@gmail.com>
2026-07-01 09:53:45 +0530

    Added 10.cpp

---
+++ DSA/array/10.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int MAX = 100;
+void rowsum(int arr[][MAX], int n,int m){
+    int sum = 0;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<3;j++){
+            sum += arr[i][j];
+        }
+        cout<<sum<<endl;
+        sum  = 0;
+
+    }
+}
+int main(){
+    int n;
+    cin>>n;
+    int m;
+    cin>>m;
+    int arr[MAX][MAX];
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            cin>>arr[i][j];
+        }
+    }
+    // int t ;
+    // cin>>t;
+    // for(int i=0;i<n;i++){
+    //     for(int j=0;j<m;j++){
+    //         if(arr[i][j]==t){
+    //             cout<<"YES"<<endl;
+    //             return 0;
+    //         }
+    //     }
+    // }
+    rowsum(arr,n, m);
+    return 0;
+
+    // cout<<"NO"<<endl;
+}
